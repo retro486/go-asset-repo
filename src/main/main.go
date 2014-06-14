8010c8dfb98d7872afc8d9bd7409cc10ef8c9e95
Russ Bernhardt <russ@rdkl.us>
2014-06-13 20:28:17 -0700

    Restructured to GOPATH format, added gorilla libraries, added secure cookie for auth tracking, log in mechanism now working as expected; onward to the actual app!

---
+++ src/main/main.go
@@ -0,0 +1,127 @@
+// OTP and base server heavily based on https://github.com/jstoja/go-otp-server
+package main
+
+import (
+  "otp"
+  "github.com/gorilla/mux"
+  "github.com/gorilla/schema"
+  "github.com/gorilla/securecookie"
+  "fmt"
+  "log"
+  "net/http"
+  "os"
+  "strconv"
+)
+
+type OTPForm struct {
+  OTP string `schema:"otp"`
+}
+
+var HMACKey = GetHMAC() // or GenerateRandomKey()
+var secureCookie = securecookie.New(HMACKey, nil)
+
+func GetPassword() string {
+  return os.Getenv("ASSET_REPO_OTP")
+}
+
+func GetHMAC() []byte {
+  return []byte(os.Getenv("ASSET_REPO_HMAC"))
+}
+
+func SetAuthCookie(w http.ResponseWriter, r *http.Request) {
+  value := map[string]bool {
+    "authorized": true,
+  }
+
+  if encoded, err := secureCookie.Encode("auth", value); err == nil {
+    cookie := &http.Cookie{
+      Name: "auth",
+      Value: encoded,
+      Path: "/",
+    }
+    http.SetCookie(w, cookie)
+  }
+}
+
+func ClearAuthCookie(w http.ResponseWriter, r *http.Request) {
+  cookie := &http.Cookie {
+    Name: "auth",
+    MaxAge: -1,
+  }
+  http.SetCookie(w, cookie)
+}
+
+func CheckAuthCookie(w http.ResponseWriter, r *http.Request) {
+  if cookie, err := r.Cookie("auth"); err == nil {
+    value := make(map[string]bool)
+    if err = secureCookie.Decode("auth", cookie.Value, &value); err == nil {
+      if value["authorized"] != true {
+        http.Redirect(w, r, "/", 302)
+      }
+    }
+  } else {
+    // cookie doesn't exist
+    http.Redirect(w, r, "/", 302)
+  }
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+  ClearAuthCookie(w, r)
+  http.Redirect(w, r, "/", 302)
+}
+
+func ShowIndex(w http.ResponseWriter, r *http.Request) {
+  // Check session key authorized, if not redirect to /
+  http.ServeFile(w, r, "views/index.html")
+}
+
+func ShowManage(w http.ResponseWriter, r *http.Request) {
+  CheckAuthCookie(w, r)
+  http.ServeFile(w, r, "views/manage.html")
+}
+
+func HandleOTP(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "POST" {
+    err := r.ParseForm()
+    if err != nil {
+      // ERROR: Unable to read form data.
+      fmt.Printf("ERROR: Bad form data\n")
+      http.Redirect(w, r, "/", 302)
+    } else {
+      otp_form := new(OTPForm)
+      decoder := schema.NewDecoder()
+      err := decoder.Decode(otp_form, r.PostForm)
+      cmp := gotp.Totp(GetPassword())
+
+      if err != nil {
+        // ERROR: Unable to decode form data.
+        fmt.Printf("ERROR: Can't decode form\n")
+      } else {
+        str_i, _ := strconv.ParseUint(otp_form.OTP, 10, 32)
+        if cmp == uint32(str_i) {
+          fmt.Printf("authorized\n")
+
+          // Set session key for authorized
+          SetAuthCookie(w, r)
+          http.Redirect(w, r, "/manage", 302)
+        } else {
+          // ERROR: Invalid OTP value given.
+          fmt.Printf("ERROR: Bad OTP\n")
+          http.Redirect(w, r, "/", 302)
+        }
+      }
+    }
+  }
+}
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/", ShowIndex)
+  r.HandleFunc("/manage", ShowManage)
+  r.HandleFunc("/requireOTP", HandleOTP)
+  r.HandleFunc("/logout", Logout)
+  http.Handle("/", r)
+
+  fmt.Printf("Listening on 0.0.0.0:8080...\n")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
